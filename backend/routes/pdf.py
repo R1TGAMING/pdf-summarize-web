c3542b92021a1a46090ee1cb835ee97c009dbb79
ipii <rafisofyangaming1234@gmail.com>
2026-04-06 20:35:36 +0700

    feat: add error handling and validation for PDF uploads in the upload route

--- backend/routes/pdf.py
+++ backend/routes/pdf.py
@@ -1,5 +1,6 @@
+from pypdf.errors import PdfStreamError
 import tempfile
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, HTTPException
 import tempfile
 from tools import load_pdf
 
@@ -8,8 +9,13 @@ router = APIRouter()
 
 @router.post("/upload")
 def upload_pdf(file: UploadFile):
-    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
-        temp_file.write(file.file.read())
-        temp_path = temp_file.name
-
-    load_pdf.invoke({"file_path": temp_path})
+    try:
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+            temp_file.write(file.file.read())
+            temp_path = temp_file.name
+            load_pdf.invoke({"file_path": temp_path})
+        return {"status_code": 200, "message": "PDF uploaded successfully"}
+    except PdfStreamError as e:
+        raise HTTPException(status_code=400, detail="Please upload a valid pdf file")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Error while uploading pdf")
